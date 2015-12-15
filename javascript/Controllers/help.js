2e9c34d082fd687476e7baaa22bad3a114e05d94
Nolan Aguirre <bobby7083@gmail.com>
2015-12-15 14:18:00 -0600

    remove unused functions

--- javascript/Controllers/help.js
+++ javascript/Controllers/help.js
@@ -5,26 +5,4 @@ angular.module( 'UILPractice' )
     function HelpController(service) {
         var vm = this;
         vm.service = service;
-        vm.submitAnswer = submitAnswer;
-        /*
-        function reverse( s ) {
-            var o = '';
-            for ( var i = s.length - 1; i >= 0; i-- )
-                o += s[ i ];
-            return o;
-        }
-        function newProblemClick() {
-            vm.service.currentProblem = vm.service.currentProblem.generate();
-            vm.service.displayAnswer = '';
-            vm.service.showHelp = false;
-        }
-        */
-        function submitAnswer() {
-            if ( vm.currentProblem.answer === 0 ) {
-                vm.displayAnswer = 'Delightful, You\'ve got it correct';
-            } else {
-                vm.displayAnswer = 'Incorrect! Correct answer is ' + vm.currentProblem.answer;
-            }
-        }
-
     }
